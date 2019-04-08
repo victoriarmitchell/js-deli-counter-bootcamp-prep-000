69fbc88ba0570e4ea889aab24a4eaf9e6b143c23
Victoria M <victoria.raq.mitchell@gmail.com>
2019-04-08 18:37:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function takeANumber(line,name) {
-for (let i = 0; i < line; i++) {
-  return `Welcome, ${name}. You are number ${i+1} in line.`;
+line.push(name)
+  return `Welcome, ${name}. You are number ${line.length} in line.`;
   }
 }
