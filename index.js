4b5f307e39a9e8780d90b7972cc63788248a7201
Victoria M <victoria.raq.mitchell@gmail.com>
2019-04-08 18:24:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 function takeANumber(line,name) {
 let katzDeliLine = [];
 for (let i = 0; i < line; i++) {
-  if i === 0
-  return `Welcome, ${name}. You 
+  if (i === 0) {
+  return `Welcome, ${name}. You are 1st in line.`;
+  } else {
+    return `Welcome, ${name}. You are ${i+1}th in line.`;
+  }
 }
 }
