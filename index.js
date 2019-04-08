380edb02a273f2de64ab156cdf24f36c370328a5
Victoria M <victoria.raq.mitchell@gmail.com>
2019-04-08 18:57:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,3 +9,11 @@ function nowServing(line) {
   }
   return `Currently serving ${line.shift()}.`
 }
+
+function currentLine(line) {
+  for (let i = 0)
+  if (!line.length) {
+    return "The line is currently empty."
+  }
+  return `The line is currently:
+}
