c4a1895291ba824f4c8740de4dcb5e0c0ebbcfdd
Victoria M <victoria.raq.mitchell@gmail.com>
2019-04-08 18:53:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ function nowServing(line) {
   if (!line.length) {
     return "There is nobody waiting to be served!"
   }
-  return `Now serving ${line.shift()}.`
+  return `Currently serving ${line.shift()}.`
 }
